c05345cd549187049fb580213cea245437cc5ad3
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-12-30 14:09:23 +0100

    update

--- whole-wavelength/src/content/config.ts
+++ whole-wavelength/src/content/config.ts
@@ -10,6 +10,8 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		name: z.string().optional(),
+		descriptionTalker: z.string().optional(),
 	}),
 });
 
